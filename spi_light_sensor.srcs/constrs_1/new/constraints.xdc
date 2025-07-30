426c12973bbe879619a329cb60148ad4075ce61d
Moafk Aljabi <mwafak_sh@hotmail.com>
2025-07-30 23:56:46 +0200

    Working on the SPI driver

---
+++ spi_light_sensor.srcs/constrs_1/new/constraints.xdc
@@ -0,0 +1,4 @@
+set_property PACKAGE_PIN G14 [get_ports {o_SPI_MOSI}]
+set_property PACKAGE_PIN H16 [get_ports {i_SPI_MISO}]
+set_property PACKAGE_PIN G13 [get_ports {o_SPI_Clk}]
+set_property PACKAGE_PIN H15 [get_ports {o_SPI_CS_n}]
--- spi_light_sensor.srcs/sources_1/new/spi_driver.vhd
+++ spi_light_sensor.srcs/sources_1/new/spi_driver.vhd
@@ -32,12 +32,64 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity spi_driver is
---  Port ( );
+    generic(
+        SPI_MODE            in  integer = 0;
+        CLKS_PER_HALF_BIT   in  integer = 12;
+    );
+
+    port (
+        -- Control Signals
+        i_Clk   : in    std_logic;
+        i_nRst  : in    std_logic
+
+        -- Transmit Signals
+        o_TX_Ready  : out   std_logic;
+        i_TX_DV     : in    std_logic;
+        i_TX_Byte   : in    std_logic_vector(7 downto 0);
+
+        -- Receive Signals
+        o_RX_DV     : out   std_logic;
+        o_RX_Byte   : out   std_logic_vector(7 downto 0);
+        
+        -- SPI Interface
+        o_SPI_Clk   : out   std_logic;
+        o_SPI_MOSI  : out   std_logic;
+        i_SPI_MISO  : in    std_logic
+    );
+
 end spi_driver;
 
 architecture RTL of spi_driver is
 
+    -- SPI Interface
+    signal w_CPOL   : std_logic;
+    signal w_CPHA   : std_logic;
+
+    -- 
+    signal r_SPI_Clk_Count      : integer range 0 to CLKS_PER_HALF_BIT *2 -1 ;
+    signal r_SPI_Clk            : std_logic;
+    signal r_SPI_Clk_Edges      : std_logic_vector range 0 to 16;
+    signal r_SPI_Leading_Edge   : std_logic;
+    signal r_SPI_Trailing_Edge  : std_logic;
+    
+    signal r_TX_DV              : std_logic;
+    signal r_TX_Byte            : std_logic;
+
+    signal r_TX_Bit_Count       : Unsigned(2 downto 0);
+    signal r_RX_Bit_Count       : Unsigned(2 downto 0);
+
 begin
+    
+    -- 
+    w_CPOL <= '1' when (SPI_MODE = 2) or (SPI_MODE = 3) else '0';
+
+    -- 
+    w_CPHA <= '1' when (SPI_MODE = 1) or (SPI_MODE = 3) else '0';
+
+
+    -- Generate SPI Clk when DV is pulled high
+    Edge_Indicator 
+
 
 
 end RTL;
